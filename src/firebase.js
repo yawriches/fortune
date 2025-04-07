7ba73424d4ff07033f0ded7556542f563546dee3
yawriches <richesad1@gmail.com>
2025-04-07 06:53:09 +0000

    logo added

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrNMs_c_QMdkJvK0kSYUDPDHG7VLbCcgk",
-  authDomain: "fortune-61ccc.firebaseapp.com",
-  projectId: "fortune-61ccc",
-  storageBucket: "fortune-61ccc.firebasestorage.app",
-  messagingSenderId: "855275069623",
-  appId: "1:855275069623:web:4ee432e3780b40b4e56d32",
-  measurementId: "G-3GHPEV6QNQ"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
